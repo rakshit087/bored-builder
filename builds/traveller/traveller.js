ad9f7f2d842d4699845290e55eddcfc069ecc8c7
Rakshit <rakshit087@gmail.com>
2022-09-19 20:10:39 +0530

    Completed traveler

--- builds/traveller/traveller.js
+++ builds/traveller/traveller.js
@@ -1,7 +1,32 @@
+class Traveller {
+  constructor(width, height) {
+    this.velocity = createVector(random(-1, 1)*3, random(-1, 1)*3);
+    this.position = createVector(width / 2, height / 2);
+  }
+  update() {
+    //if wall is hit, reverse velocity
+    if (this.position.x > width || this.position.x < 0) {
+      this.velocity.x = this.velocity.x * -1;
+    }
+    if (this.position.y > height || this.position.y < 0) {
+      this.velocity.y = this.velocity.y * -1;
+    }
+    this.position.add(this.velocity);
+  }
+  show() {
+    stroke(255);
+    strokeWeight(2);
+    ellipse(this.position.x, this.position.y, 5, 5);
+  }
+}
+
 function setup() {
   createCanvas(400, 400);
+  t = new Traveller(width, height);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  t.update();
+  t.show();
 }
