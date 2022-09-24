42c37a217c36ffe9f396f41971a4417cb762bf52
Rakshit <rakshit087@gmail.com>
2022-09-24 14:01:18 +0530

    Reseting acceleration

--- builds/ball/ball.js
+++ builds/ball/ball.js
@@ -1,7 +1,7 @@
 class World {
   constructor(gravity, wind) {
-    this.gravity = createVector(0,gravity);
-    this.wind = createVector(wind,0);
+    this.gravity = createVector(0, gravity);
+    this.wind = createVector(wind, 0);
   }
 }
 
@@ -16,36 +16,46 @@ class Ball {
   }
   update() {
     // Reverse velocity if ball hits the wall
-    if (this.position.x > width - this.radius || this.position.x < this.radius) {
-      this.position.x = constrain(this.position.x, this.radius, width - this.radius);
+    if (
+      this.position.x > width - this.radius ||
+      this.position.x < this.radius
+    ) {
+      this.position.x = constrain(
+        this.position.x,
+        this.radius,
+        width - this.radius
+      );
       this.velocity.x *= -1;
     }
     // Reverse velocity if ball hits the floor / ceiling
-    if(this.position.y > height - this.radius) {
-      this.position.y = constrain(this.position.y, this.radius, height - this.radius);
+    if (this.position.y > height - this.radius) {
+      this.position.y = constrain(
+        this.position.y,
+        this.radius,
+        height - this.radius
+      );
       this.velocity.y *= -1;
     }
     this.velocity.add(this.acceleration);
     this.position.add(this.velocity);
+    this.acceleration.set(0,0);
   }
 
   applyForce(force) {
-    this.acceleration.add(
-      p5.Vector.div(force, this.mass)
-    );
+    this.acceleration.add(p5.Vector.div(force, this.mass));
   }
 
   show() {
     stroke(0, 255, 0);
     strokeWeight(1);
-    ellipse(this.position.x, this.position.y, this.radius*2, this.radius*2);
+    ellipse(this.position.x, this.position.y, this.radius * 2, this.radius * 2);
     fill(0, 255, 0);
   }
 }
 
 function setup() {
   createCanvas(400, 400);
-  world = new World(0.01, 0.05);
+  world = new World(0.2, 0.05);
   t1 = new Ball(100, 0, 1, world);
   t2 = new Ball(300, 0, 5, world);
 }
@@ -54,7 +64,7 @@ function draw() {
   background(0);
   t1.applyForce(t1.weight);
   t2.applyForce(t2.weight);
-  if(mouseIsPressed) {
+  if (mouseIsPressed) {
     t1.applyForce(world.wind);
     t2.applyForce(world.wind);
   }
